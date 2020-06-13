99217f69218e924de3b1558b04fac91c98a2f7dd
沈北辰 <1150311203@qq.com>
2020-06-13 22:57:26 +0900

    perfomance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,24 +17,36 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item,key) of cities" :key="key">
+      <div
+        class="area" v-for="(item,key) of cities"
+        :key="key"
+        :ref="key"
+      >
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list" v-for="innerItem of item" :key="innerItem.id">
           <div class="item border-bottom">{{innerItem.name}}</div>
         </div>
       </div>
-    </div>>
+    </div>
   </div>
 </template>
 <script>
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
-  props: ['cities', 'hotCities'],
+  props: ['cities', 'hotCities', 'letter'],
   mounted () {
     this.$nextTick(() => {
-      this.scroll = new BScroll(this.$refs.wrapper, {})
+      this.scroll = new BScroll(this.$refs.wrapper)
     })
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
